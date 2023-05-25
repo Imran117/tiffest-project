fc6cdd736a04d4102c263d0a107fffa9b9c9ad3b
Imran <umurzakov117ali@gmail.com>
2023-05-25 14:13:32 +0500

    start new component Navbar

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="content">
+    <Navbar />
+  </div>
+</template>
+
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+export default {
+  components: {
+    Navbar
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss">
+@import "@/assets/styles/main.scss";
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+    <header class="header">
+        <nav class="header__nav">
+            <button class="header__nav-logo">
+                <img src="@/assets/images/logo.png" alt="">
+            </button>
+            <ul>
+                <li><a href="#!" @click.prevent="">TIFFEST-22</a></li>
+                <li><a href="#!" @click.prevent="">Программа</a></li>
+                <li><a href="#!" @click.prevent="">Кинопоказы</a></li>
+                <li><a href="#!" @click.prevent="">Локации</a></li>
+                <li><a href="#!" @click.prevent="">Премьеры</a></li>
+                <li><a href="#!" @click.prevent="">Лица</a></li>
+                <li><a href="#!" @click.prevent="">Медиа</a></li>
+                <li><a href="#!" @click.prevent="">История</a></li>
+                <li><a href="#!" @click.prevent="">Мастер-классы</a></li>
+                <li><a href="#!" @click.prevent="">Новости</a></li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+</style>
