504bb8953bd071b325a74d97a6d4a0bf81661f77
Imran <umurzakov117ali@gmail.com>
2023-05-26 16:10:41 +0500

    finished animated effect for home page

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import store from '@/store/index.js'
 import router from '@/router/index.js'
 const app = createApp(App)
 
-
 app.use(router)
 app.use(store)
 app.mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,42 +1,67 @@
 <template>
-    <section class="home" @mousemove="getmouseMove">
-        <div class="home__back">
-         <!-- <div data-speed="10" class="home__back-layer layer1"></div>
-         <div data-speed="20" class="home__back-layer layer2"></div>
-         <div data-speed="30" class="home__back-layer layer3"></div>
-         <div data-speed="40" class="home__back-layer layer4"></div>  -->
-         <img data-speed="1" class="home__back-layer layer1" src="@/assets/images/parallax-image/parallax-vklad1.png" alt="">
-         <img data-speed="2" class="home__back-layer layer2" src="@/assets/images/parallax-image/parallax-vklad2.png" alt="">
-         <img data-speed="3" class="home__back-layer layer3" src="@/assets/images/parallax-image/parallax-vklad3.png" alt="">
-         <!-- <img data-speed="4" class="home__back-layer layer4" src="@/assets/images/parallax-image/parallax-vklad4.png" alt=""> -->
-         <div class="home__back-blur"></div>
-        </div>
-    </section>
+  <section class="home" @mousemove="getmouseMove">
+    <div class="home__back">
+      <img
+        data-speed="1"
+        class="home__back-layer layer1"
+        src="@/assets/images/parallax-image/parallax-vklad1.png"
+        alt=""
+      />
+      <img
+        data-speed="2"
+        class="home__back-layer layer2"
+        src="@/assets/images/parallax-image/parallax-vklad2.png"
+        alt=""
+      />
+      <img
+        data-speed="3"
+        class="home__back-layer layer3"
+        src="@/assets/images/parallax-image/parallax-vklad3.png"
+        alt=""
+      />
+      <div class="home__back-blur"></div>
+    </div>
+    <div class="home__content">
+      <div class="home__content-statue">
+        <img
+          src="@/assets/images/parallax-image/parallax-kub.png"
+          alt=""
+        />
+      </div>
+
+      <div class="home__content-txt">
+        <h1 class="home__content-title">XIV</h1>
+        <p class="home__content-subtitle">
+          Ташкентский международный кинофестиваль
+        </p>
+        <p class="home__content-subtext">“Жемчужина шелкового пути”</p>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
+  data() {
+    return {};
+  },
+  methods: {
+    getmouseMove(event) {
+      const elem = document.querySelectorAll(".home__back-layer");
+      const clientX = event.clientX,
+        clientY = event.clientY;
 
-    data() {
-        return {}
+      elem.forEach((el) => {
+        const attr = el.getAttribute("data-speed");
+        el.style.left = `${(clientX * attr) / 100}px`;
+        el.style.top = `${(clientY * attr) / 100}px`;
+      });
     },
-    methods: {
-        getmouseMove(event) {
-            const elem = document.querySelectorAll(".home__back-layer")
-            const clientX = event.clientX,
-                    clientY = event.clientY;
-            
-                    elem.forEach(el => {
-                        const attr = el.getAttribute('data-speed')
-                        el.style.left = `${clientX*attr/100}px`
-                        el.style.top = `${clientY*attr/100}px`
-                    })
-        }
-    }
+  },
 
-}
+  created() {}
+};
 </script>
 
 <style>
-
 </style>
